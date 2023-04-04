1a14199ad3f2cd9235f834d0a2e955f4c81aab70
Pedro Cavalcante <pedrocavalcantea02@gmail.com>
2023-04-04 14:10:24 -0300

    adicionando ConfigSecurity, autenticacao via base de dados

---
+++ src/main/java/com/api/parkingcontrol/configs/security/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.api.parkingcontrol.configs.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.models.UserModel;
+import com.api.parkingcontrol.repositories.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+	final UserRepository userRepository;
+
+	public UserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		UserModel userModel = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("user not found with username: " + username)
+
+				);
+		return userModel;
+	}
+
+}
--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.api.parkingcontrol.configs;
+package com.api.parkingcontrol.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
